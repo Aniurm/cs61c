28f368d009fb28f2d9c31b78d1dcaa1e095b91a5
Aniurm <534147085@qq.com>
2023-02-23 21:43:35 +0800

    完成一半lab2

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -5,23 +5,33 @@
 // Assume 0 <= n <= 31
 unsigned get_bit(unsigned x,
                  unsigned n) {
-    // YOUR CODE HERE
-    // Returning -1 is a placeholder (it makes
-    // no sense, because get_bit only returns
-    // 0 or 1)
-    return -1;
+    // move the nth bit to the least significant position
+    x = x >> n;
+    // mask off all but the least significant bit
+    return x & 1;
 }
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
 void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
-    // YOUR CODE HERE
+    // make a mask to clear the nth bit of x
+    unsigned mask = ~(1 << n);
+    // clear the nth bit of x
+    *x = *x & mask;
+    // make a mask to set the nth bit of x to v
+    mask = (v << n);
+    // set the nth bit of x to v
+    *x = *x | mask;
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
 void flip_bit(unsigned * x,
               unsigned n) {
-    // YOUR CODE HERE
+    // get the nth bit of x
+    unsigned nthBit = get_bit(*x, n);
+    // flip the nth bit
+    nthBit = nthBit ^ 1;
+    // set the nth bit of x to the flipped bit
+    set_bit(x, n, nthBit);
 }
-
