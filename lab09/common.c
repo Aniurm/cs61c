f09ad83d8551b73586bcce808f027503e47b635a
Aniurm <534147085@qq.com>
2023-05-12 10:57:16 +0800

    Finish Lab9

--- lab09/common.c
+++ lab09/common.c
@@ -75,11 +75,30 @@ long long int sum_simd_unrolled(unsigned int vals[NUM_ELEMS]) {
 	__m128i _127 = _mm_set1_epi32(127);
 	long long int result = 0;
 	for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
-		/* COPY AND PASTE YOUR sum_simd() HERE */
-		/* MODIFY IT BY UNROLLING IT */
+		for (unsigned int i = 0; i < NUM_ELEMS / 16 * 16; i += 16) {
+			__m128i vector1 = _mm_loadu_si128((__m128i *) (vals + i));
+			__m128i vector2 = _mm_loadu_si128((__m128i *) (vals + i + 4));
+			__m128i vector3 = _mm_loadu_si128((__m128i *) (vals + i + 8));
+			__m128i vector4 = _mm_loadu_si128((__m128i *) (vals + i + 12));
+			__m128i mask1 = _mm_cmpgt_epi32(vector1, _127);
+			__m128i mask2 = _mm_cmpgt_epi32(vector2, _127);
+			__m128i mask3 = _mm_cmpgt_epi32(vector3, _127);
+			__m128i mask4 = _mm_cmpgt_epi32(vector4, _127);
+			__m128i masked1 = _mm_and_si128(vector1, mask1);
+			__m128i masked2 = _mm_and_si128(vector2, mask2);
+			__m128i masked3 = _mm_and_si128(vector3, mask3);
+			__m128i masked4 = _mm_and_si128(vector4, mask4);
+			result += _mm_extract_epi32(masked1, 0) + _mm_extract_epi32(masked1, 1) + _mm_extract_epi32(masked1, 2) + _mm_extract_epi32(masked1, 3);
+			result += _mm_extract_epi32(masked2, 0) + _mm_extract_epi32(masked2, 1) + _mm_extract_epi32(masked2, 2) + _mm_extract_epi32(masked2, 3);
+			result += _mm_extract_epi32(masked3, 0) + _mm_extract_epi32(masked3, 1) + _mm_extract_epi32(masked3, 2) + _mm_extract_epi32(masked3, 3);
+			result += _mm_extract_epi32(masked4, 0) + _mm_extract_epi32(masked4, 1) + _mm_extract_epi32(masked4, 2) + _mm_extract_epi32(masked4, 3);
+		}
 
-		/* You'll need 1 or maybe 2 tail cases here. */
-
+		for (unsigned int i = NUM_ELEMS / 16 * 16; i < NUM_ELEMS; i++) {
+			if (vals[i] >= 128) {
+				result += vals[i];
+			}
+		}
 	}
 	clock_t end = clock();
 	printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
