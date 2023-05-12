68d48913c2c0d5a654cfc406fb0deaebab25951b
Aniurm <534147085@qq.com>
2023-05-12 10:29:28 +0800

    Lab9: Finish Exercise 2

--- lab09/common.c
+++ lab09/common.c
@@ -52,11 +52,19 @@ long long int sum_simd(unsigned int vals[NUM_ELEMS]) {
 	/* DO NOT DO NOT DO NOT DO NOT WRITE ANYTHING ABOVE THIS LINE. */
 	
 	for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
-		/* YOUR CODE GOES HERE */
-
-		/* You'll need a tail case. */
-
+		for (unsigned int i = 0; i < NUM_ELEMS / 4; i += 1) {
+			__m128i vector = _mm_loadu_si128((__m128i *) (vals + i * 4));
+			__m128i mask = _mm_cmpgt_epi32(vector, _127);
+			__m128i masked = _mm_and_si128(vector, mask);
+			result = _mm_extract_epi32(masked, 0) + _mm_extract_epi32(masked, 1) + _mm_extract_epi32(masked, 2) + _mm_extract_epi32(masked, 3);
+		}
+		for (unsigned int i = NUM_ELEMS / 4 * 4; i < NUM_ELEMS; i++) {
+			if (vals[i] >= 128) {
+				result += vals[i];
+			}
+		}
 	}
+
 	clock_t end = clock();
 	printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
 	return result;
